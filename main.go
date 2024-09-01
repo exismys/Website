0112204897d04371583a9b96ad0ffff5c774b3e7
Ritesh Patel <riteshzeronity@gmail.com>
2024-09-02 00:41:53 +0530

    Setup route for reading blogs

--- main.go
+++ main.go
@@ -5,7 +5,23 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"strings"
 )
+
+func main() {
+	// Handle routes
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", handlerHome)
+	http.HandleFunc("/blog/", handlerBlog)
+
+	// Start server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Printf("Listening at http://localhost:%s\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+}
 
 func handlerHome(w http.ResponseWriter, r *http.Request) {
 	blogTitles := getBlogList()
@@ -19,13 +35,24 @@ func handlerHome(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, blogTitles)
 }
 
-func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", handlerHome)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+func handlerBlog(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	fmt.Printf("Blog handler called with path %s\n", path)
+	parts := strings.Split(path, "/")
+	if len(parts) < 3 {
+		http.NotFound(w, r)
+		return
 	}
-	fmt.Printf("Listening to port %s", port)
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+
+	blogFile := fmt.Sprintf("blogs/%s.html", parts[2])
+	blog := getBlogPost(blogFile)
+
+	t, err := template.ParseFiles("templates/blog.html")
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		fmt.Fprintf(w, "Unable to load template")
+		return
+	}
+
+	t.Execute(w, blog)
 }
